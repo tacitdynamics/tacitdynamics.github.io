161557cdf5b4e078125a6afc7d7d26f057b575d4
Anders Carlsen <acarlsen@gmail.com>
2020-07-23 20:08:23 +0200

    Removed button

--- _includes/js/scripts.js
+++ _includes/js/scripts.js
@@ -1,21 +1,3 @@
-function copyToClipboard(copyText) {
-    var dummy = document.createElement("textarea");
-    // to avoid breaking orgain page when copying more words
-    // cant copy when adding below this code
-    // dummy.style.display = 'none'
-    document.body.appendChild(dummy);
-    //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
-    dummy.value = copyText;
-    dummy.select();
-    document.execCommand("copy");
-    document.body.removeChild(dummy);
-  
-    /* Alert the copied text */
-    alert("Copied the code: " + copyText);
-    }   
-
-
 const urlParams = new URLSearchParams(window.location.search);
 const myParam = urlParams.get('code');
 document.getElementById("myworkaround").innerHTML = "OAuth code is: <b>" + myParam + "</b><br/><br/>";
-document.getElementById("myworkaround-copy").innerHTML = "<button onclick='copyToClipboard(" + myParam + ")'>Copy code to clipboard</button><br/><br/>";
--- oauth-return.md
+++ oauth-return.md
@@ -16,7 +16,4 @@ From version 3.0.x there is a fallback option, that can be used by copy/pasting 
 
 <div id="myworkaround">OAuth code is: N/A</div>
 
-<div id="myworkaround-copy"></div>
-
-
 Copy above code to clipboard, press back button until you see account screen in FolderSync, press fallback button and enter code.
