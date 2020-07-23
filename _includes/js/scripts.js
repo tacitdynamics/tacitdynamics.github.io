087b663c5cf4822a66899fd4e0855e434186fe21
Anders Carlsen <acarlsen@gmail.com>
2020-07-23 19:58:21 +0200

    Fix JS

--- _includes/js/scripts.js
+++ _includes/js/scripts.js
@@ -1,10 +1,14 @@
 function copyToClipboard(copyText) {
-    const el = document.createElement('textarea');
-    el.value = copyText;
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
+    var dummy = document.createElement("textarea");
+    // to avoid breaking orgain page when copying more words
+    // cant copy when adding below this code
+    // dummy.style.display = 'none'
+    document.body.appendChild(dummy);
+    //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
+    dummy.value = copyText;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
   
     /* Alert the copied text */
     alert("Copied the code: " + copyText);
